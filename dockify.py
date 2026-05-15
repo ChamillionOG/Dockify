d0bb4502f3e21f359528d1487ee6db034987405c
Chamillion <chamillionog@gmail.com>
2026-05-14 22:53:27 -0400

    Redid Container Code + Added Song Name Label

--- dockify.py
+++ dockify.py
@@ -1,12 +1,19 @@
-import sys
-
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLabel
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtGui import QIcon, QFontDatabase
 from PyQt5.QtCore import Qt
 
 class DockifyUI(QWidget):
     def __init__(self):
         super().__init__()
+
+        font_id = QFontDatabase.addApplicationFont("assets/default_font.ttf")
+
+        if font_id != -1:
+            self.font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        else:
+            self.font_family = "Arial"
+
+        print(self.font_family)
 
         self.setup_window()
         self.setup_ui()
@@ -14,8 +21,8 @@ class DockifyUI(QWidget):
         self.setStyleSheet("""
             QWidget {
                 background-color: rgba(18, 18, 18, 255);
-                border: 2px solid rgba(255, 255, 255, 255);
-                border-radius: 40px;
+                border: 0.5px solid rgba(255, 255, 255, 255);
+                border-radius: 25px;
             }
 
             QPushButton {
@@ -24,50 +31,80 @@ class DockifyUI(QWidget):
                 border-radius: 10px;
                 padding: 6px;
             }
+                           
+            QLabel {
+                font-family: "{self.font_family}";
+                background-color: transparent;
+                font-weight: bold;
+                font-size: 15px;
+                border: none;
+                color: white;
+            }
 
             QPushButton:hover {
                 color: #1DB954;
             }
         """)
-
+    
     def setup_window(self):
         self.setWindowTitle("Dockify")
-        self.setFixedSize(300, 80)
+        self.setFixedSize(350, 90)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
     def setup_ui(self):
         self.container = QWidget(self)
 
-        layout = QHBoxLayout(self.container)
-        layout.setContentsMargins(10, 10, 10, 10)
-        layout.setSpacing(8)
+        main_layout = QHBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+        main_layout.addWidget(self.container)
 
-        self.album_cover = QLabel()
-        pixmap = QPixmap("assets/default_album_cover.png")
-        self.album_cover.setPixmap(pixmap)
-        self.album_cover.setFixedSize(40, 40)
-        self.album_cover.setScaledContents(True)
+        container_layout = QVBoxLayout()
+        container_layout.setContentsMargins(75, 10, 75, 10)
+        container_layout.setSpacing(5)
 
+        # Buttons
+        button_layout = QHBoxLayout()
+
+        self.shuffle_button = QPushButton()
         self.previous_button = QPushButton()
         self.play_button = QPushButton()
         self.next_button = QPushButton()
+        self.repeat_button = QPushButton()
 
+        self.shuffle_button.setIcon(QIcon("assets/shuffle_button_icon.png"))
         self.previous_button.setIcon(QIcon("assets/previous_button_icon.png"))
         self.play_button.setIcon(QIcon("assets/play_button_icon.png"))
         self.next_button.setIcon(QIcon("assets/next_button_icon.png"))
+        self.repeat_button.setIcon(QIcon("assets/repeat_button_icon.png"))
 
+        self.shuffle_button.setFixedSize(30, 30)
         self.previous_button.setFixedSize(30, 30)
         self.play_button.setFixedSize(35, 35)
         self.next_button.setFixedSize(30, 30)
+        self.repeat_button.setFixedSize(30, 30)
 
-        layout.addWidget(self.album_cover)
-        layout.addStretch()
-        layout.addWidget(self.previous_button)
-        layout.addWidget(self.play_button)
-        layout.addWidget(self.next_button)
-        layout.addStretch()
+        button_layout.addWidget(self.shuffle_button)
+        button_layout.addWidget(self.previous_button)
+        button_layout.addWidget(self.play_button)
+        button_layout.addWidget(self.next_button)
+        button_layout.addWidget(self.repeat_button)
 
-        main_layout = QHBoxLayout(self)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        main_layout.addWidget(self.container)
+        # Text
+        text_layout = QVBoxLayout()
+
+        self.song_label = QLabel("Song Name Here")
+        self.song_label.setAlignment(Qt.AlignCenter)
+
+        self.artist_label = QLabel("Artist Name")
+        self.artist_label.setAlignment(Qt.AlignCenter)
+
+        text_layout.addWidget(self.song_label)
+        #text_layout.addWidget(self.artist_label)
+
+        # Set Up
+        container_layout.addLayout(text_layout)
+        container_layout.addLayout(button_layout)
+
+        self.container.setLayout(container_layout)
